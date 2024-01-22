24cf7eb541126c61344b4fa937587fe93dce43e9
Patrick Njiru <patricknjiru93@gmail.com>
2024-01-22 23:26:46 +0300

    complete the self assigned records modifier code challenge

--- coding-challenges.js
+++ coding-challenges.js
@@ -121,25 +121,12 @@ const updateRecords = (id, prop, value) => {
 
 // console.log(collection);
 
-function recordsModifier2(
-    id, 
-    props={
-        album: '',
-        artist: '',
-        tracks: []
-    },  
-    action) {
+function recordsModifier2(id, props, action) {
         
     // Only accept positive integers
     if (typeof id !== 'number' || id < 0 || typeof props !== 'object' || 
     (action !== 'create' && action !== 'read' && action !== 'update' && action !== 'delete' && action !== 'delete props') ) {
-        return (
-            `Invalid input(s). 
-            Order of inputs: (id, props, action)
-
-            id - positive integer,
-            props - an object - {album: '', artist: '', tracks: []},
-            action - create/read/update/delete/delete props`)
+        return (`\tInvalid input(s). \n\tOrder of inputs: (id, props, action) \n\tid - positive integer, \n\tprops - an object - {album: '', artist: '', tracks: []}, \n\taction - create/read/update/delete/delete props`)
         }
 
     Math.floor(id)
@@ -147,55 +134,86 @@ function recordsModifier2(
     switch(action) {
         case 'create':
             if (collection[id]) {
-                return "A Record with this ID already exists! Cannot create record."            
+                return `Record ${id} Already Exists! Cannot Create Record.`
+            } else if(Object.keys(props).length < 1) {
+                collection[id] = {}
+                props = { album: '', artist: '', tracks: [] }
+
+                for (const prop in props) {
+                    // Object.assign(collection,{props[prop]}) // adding a new propeties to an object
+                    collection[id][prop] = props[prop]
+                }
+
+                console.log(`Record ${id} Created Successfully`);
+            } else {
+                collection[id] = {}
+                
+                for (const prop in props) {
+                    if (prop !== 'tracks' && prop !== 'artist' && prop !== 'album' ) {
+                        return `Invalid Property - ${prop}. Allowed Properties are 'artist', 'album' and 'tracks'.`
+                    }
+                    collection[id][prop] = props[prop]              
+                }
+
+                console.log(`Record ${id} Created Successfully`);
+            }
+            return collection
+        case 'read':
+            return (collection[id] ? collection[id] : 'This Record Does Not Exist Enter a Valid ID')
+        case 'update':
+            if (Object.keys(props).length < 1) {
+                return "You must add properties to update"
             } else {
                 for (const prop in props) {
-                    if (props) {
-                    }
-                    if(prop === 'tracks') {
-                        collection[id] = {}
-                        collection[id][prop] = []
-                        collection[id][prop].push(collection[id][prop])
-                    } 
-                    collection[id] = {}
-                    collection[id][prop] = collection[id][prop]
-                }
-                return collection
-            }
-        case 'read':
-            return (id ? collection[id] : 'This Record Does Not Exist Enter a Valid ID')
-        case 'update':
-            for (const prop in props) {
-                    if (prop === 'tracks') {
-                        collection[id][prop].push(collection[id][prop])
+                    if (prop !== 'tracks' && prop !== 'artist' && prop !== 'album' ) {
+                        return `Invalid Property - ${prop}. Allowed Properties are 'artist', 'album' and 'tracks'.`
+                    } else if (prop === 'tracks') {
+                        if(typeof props[prop] !== 'object') {
+                            return "The Value of 'tracks' Must be an Array Containing songs"
+                        }
+
+                        for (const track of props[prop]) {
+                            collection[id][prop].push(track)
+                        }
                     } else {
-                        collection[id][prop] = collection[id][prop]
+                        collection[id][prop] = props[prop]
                     }
+                }
+                console.log(`Record ${id} Updated Successfully`);
             }
+            return collection
         case 'delete':
             if (!collection[id]) {
                 return 'Record does not exist. Enter a Valid ID'
             } else {
                 delete collection[id]
-                console.log('Record Deleted Successfully.');
+                console.log(`Record ${id} Deleted Successfully.`);
                 return collection
             }
         case 'delete props':
             for (const prop in props) {
-                    delete collection[id][prop]
+                if (prop !== 'tracks' && prop !== 'artist' && prop !== 'album' ) {
+                    return `Invalid Property - ${prop}. Allowed Properties are 'artist', 'album' and 'tracks'.`
+                }
+                delete collection[id][prop]
+                console.log(`${prop} Property Deleted Successfully`);
                 }
             return collection[id]
     }
 }
 
 // recordsModifier1('', 'artist', 'Eminem')
-console.log(recordsModifier2(1050, {}, 'create'))
-// console.log(recordsModifier2(1050, 'tracks', "Godzilla", 'update'))
-// console.log(recordsModifier2(1050, 'tracks', "Farewell", 'update'))
-// console.log(recordsModifier2(1050, 'tracks', "Yah Yah", 'update)'))
-// console.log(recordsModifier2(1050, 'tracks', "Asshole", 'update)'))
-// console.log(recordsModifier2(1245, 'album', "ABC", 'update'))
-// console.log(recordsModifier2(5439, 'delete'))
-// console.log(recordsModifier2(1050, 'tracks', 'asshole' , 'delete'))
+// console.log(recordsModifier2(1050, {artist: 'Eminem', tracks: ["Godzilla", "Farewell", "Yah Yah" ], album: 'MTBMB'}, 'create')) // works!
+// console.log();
+// console.log(recordsModifier2(1050, {}, 'read')); // works!
+// console.log();
+// console.log(recordsModifier2(1245, {'album': "ABC", tracks: [1,2,3]}, 'update'))
+// console.log();
+// console.log(recordsModifier2(1245, {tracks: ['a','b','c']}, 'update')) // works!
+// console.log();
+// console.log(recordsModifier2(1050, {trancks: ["Asshole"]}, 'update')) // works
+// console.log();
+// console.log(recordsModifier2(1050, {tracks: ''} , 'delete props')) // works!
+// console.log();
+// console.log(recordsModifier2(5439, {}, 'delete')) // works
 
-// Incomplete
